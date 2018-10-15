242875823f6ec6121c7ac1141ace00b04e756328
Travis Jones <suigenerisbeatz@gmail.com>
2018-10-15 12:13:17 -0500

    new

---
+++ power-up/open.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+import numpy
+#this is the begining of my unstall script"
+
+print('this is a test to see if i know what i am doing ')
+
+def test():
+    print("this is a test")
+test()
